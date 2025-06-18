f866bbdc9e2444cde9b03be49809fad3f56c3652
Nico01210 <nicolas.perret@le-campus-numerique.fr>
2025-06-18 12:18:22 +0200

    change Footer

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer class="bg-light text-center text-lg-start mt-auto">
     <div class="text-center p-3">
         © 2021 Copyright:
-        <a class="text-dark" href="https://openclassrooms.com/">OpenClassrooms</a>
+        <a class="text-dark" href="https://nicolas-perret.com/">Perret Nicolas</a> via
+        <a class="text-dark" href="https://openclassrooms.com/">OpenClassRooms</a>
     </div>
 </footer>
